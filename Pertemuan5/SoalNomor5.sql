073f6493032c21fff7b31dcf5bb9caf22a323b28
AleenBoy <145645779+AleenBoy@users.noreply.github.com>
2024-10-23 15:27:21 +0700

    Add files via upload

---
+++ Pertemuan5/SoalNomor5.sql
@@ -0,0 +1,12 @@
+INSERT INTO dosen_wali (nip, nama)
+VALUES 
+('358958', 'Aminudin'),
+('123345', 'Maarlo'),
+('165456', 'Asep'),
+('512345' 'Mihaw'),
+('325367', 'Owim'),
+('098579', 'Bahlil'),
+('237850', 'Micky'),
+('123499', 'Paul'),
+('248599', 'Puan'),
+('993854', 'Kisame'),
